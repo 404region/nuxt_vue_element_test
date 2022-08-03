10c9aaf47f7703c3c65c3660cd9903b68afcdea8
Anna Serdukova <404region@gmail.com>
2022-08-03 18:29:10 +0300

    Initial commit

---
+++ pages/login.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <h1>Авторизация</h1>
+        <form @submit="userLogin">
+            <div>
+                <label>Электронная почта</label>
+                <input type="email" v-model="login.mail">
+            </div>
+            <div>
+                <label>Пароль</label>
+                <input type="password" v-model="login.password" minlength="8">
+            </div>
+            <el-button type="primary" plain>Авторизация</el-button>
+        </form>
+    </div>
+    
+    
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                login: {
+                    mail: '',
+                    password: ''
+                }
+            }
+        },
+        methods: {
+            userLogin() {
+                console.log('login');
+            }
+        }
+    }
+    
+</script>
