b44ee7c5e0731677480beab3be10c7021b0cda53
Anna Serdukova <404region@gmail.com>
2022-08-05 05:40:06 +0300

    Добавлено меню

---
+++ components/Nav-vue.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal">
+            <el-menu-item index="index" keyPath="/"><router-link to="/">Главная</router-link></el-menu-item>
+            <el-menu-item index="login" keyPath="/login"><router-link to="/login">Авторизация</router-link></el-menu-item>
+        </el-menu>
+    </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+            activeIndex: 'index',
+            activeIndex2: 'login'
+      };
+    },
+    methods: {
+    }
+  }
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,8 @@
 <template>
-  <Tutorial />
+  <NavVue/>
 </template>
-  <div>
-    <h1>Главная страница</h1>
-  </div>
 <script>
-export default {
-  name: 'IndexPage',
-}
+  export default {
+    name: 'IndexPage'
+  }
 </script>
